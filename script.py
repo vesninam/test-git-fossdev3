376dac1731a3dab49288efcaed3c6d2d45deeed5
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-05 06:57:25 +0000

    Restricted devision of strings

--- script.py
+++ script.py
@@ -4,5 +4,8 @@ def sum(a, b):
 def devide(a, b):
     if b == 0:
         raise ValueError("Denominator could not be zero")
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("Could not devide strings")
+
     return a / b
 
