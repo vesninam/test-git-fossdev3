6a62a1101f25182f151f2214c18bcd994019a3ac
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-10 07:04:27 +0000

    Refactored ndfl calcualation

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,31 +1,16 @@
 def calculate_ndfl(income):
     result = 0
-    if income < 2_400_000:
-        result = income * 0.13
-    elif income < 5_000_000:       
-        result = (
-            2_400_000 * 0.13 + 
-            (income - 2_400_000) * 0.15
-        )
-    elif income < 20_000_000:       
-        result = (
-            2_400_000 * 0.13 + 
-            2_600_000 * 0.15 +
-            (income - 5_000_000) * 0.18
-        )
-    elif income < 50_000_000:       
-        result = (
-            2_400_000 * 0.13 + 
-            2_600_000 * 0.15 +
-            15_000_000 * 0.18 +
-            (income - 20_000_000) * 0.20
-        )
-    elif income > 50_000_000:       
-        result = (
-            2_400_000 * 0.13 + 
-            2_600_000 * 0.15 +
-            15_000_000 * 0.18 +
-            30_000_000 * 0.20 +
-            (income - 50_000_000) * 0.22
-        )
-    return result
+    # start addition taxrate
+    tiers = [
+        (0, 0, 0.13),
+
+        (2_400_000, 312_000, 0.15),
+        (5_000_000, 702_000, 0.18),
+        (20_000_000, 3_402_000, 0.20),
+        (50_000_000, 9_402_000, 0.22),
+    ]
+    for start, addition, taxrate in tiers[::-1]:
+        if income > start:
+            return (income - start) * taxrate + addition
+    raise RuntimeError(f"Error in tax calculation {income}")
+    
