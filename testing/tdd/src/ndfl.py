8fee7147a8cdd2632aa1b4a9c4da43a0efbbb7fe
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-10 06:42:16 +0000

    Added boundary income testpass

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,2 +1,12 @@
 def calculate_ndfl(income):
-    return income * 0.13
+    result = 0
+    if income < 2_400_000:
+        result = income * 0.13
+    else:       
+
+        result = (
+            2_400_000 * 0.13 + 
+            (income - 2_400_000) * 0.15
+        )
+
+    return result
